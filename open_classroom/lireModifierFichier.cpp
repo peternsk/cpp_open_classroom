51dc78cb4b48b50cee84e9c7e6baa406e25ae26d
Peter Malonge Nsaka <pnsaka@c1r8p4.42quebec.com>
2024-03-12 02:10:36 -0400

    lire et modifier un fichier

--- open_classroom/lireModifierFichier.cpp
+++ open_classroom/lireModifierFichier.cpp
@@ -7,20 +7,34 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
+
+// int main()
+// {
+//     //std::ofstream monflux("texte.txt");
+//     std::string const nomFichier("texte.txt");
+//     //Déclaration d'un flux permettant d'écrire dans le fichier
+//     // C:/Nanoc/scores.txt
+
+//     std::ofstream monFlux(nomFichier.c_str());
+//     if(monFlux)
+//         std::cout << "all good" << std::endl;
+//     else
+//         std::cout << "ERREUR : ouverture de fichier" << std::endl;
+
+//     //apres la verificartion on peu
+//     return 0;
+// }
 
 int main()
 {
-    //std::ofstream monflux("texte.txt");
-    std::string const nomFichier("texte.txt");
-    //Déclaration d'un flux permettant d'écrire dans le fichier
-    // C:/Nanoc/scores.txt
+    std::string const myfile("text.txt");
+    std::ofstream monflux(myfile.c_str());
 
-    std::ofstream monFlux(nomFichier.c_str());
-    if(monFlux)
-        std::cout << "all good" << std::endl;
-    else
-        std::cout << "ERREUR : ouverture de fichier" << std::endl;
+    if(monflux){
+        monflux << "bonjour comment ca va"
+    }
+    else{
 
-    //apres la verificartion on peu
-    return 0;
+    }
 }
