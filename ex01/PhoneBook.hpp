4b53e4ada9b51cd8d93ee302158d8769ad1d2c5a
Peter Nsaka <peternsaka@Peters-MacBook-Pro.local>
2024-03-20 19:04:04 -0400

    ex01

---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,14 @@
+#ifndef __PHONEBOOK_H__
+#define __PHONEBOOK_H__
+
+/************************************************/
+/**  				  CLASS					   **/
+/************************************************/
+
+class	PhoneBook{
+	private:
+
+	public:
+};
+
+#endif
